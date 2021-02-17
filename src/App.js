dce7f17eda5ff18999873b384b9d010f4818a308
vinay <vinay0458@gmail.com>
2021-02-16 21:55:03 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/header/Header";
+import RepositoryPage from './pages/repository-page/RepositoryPage'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <RepositoryPage />
+    </div>
+  );
+}
+
+export default App;
